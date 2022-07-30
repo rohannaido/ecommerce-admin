2131c47162a8557015f918fcadd959a3ab189291
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-07-30 22:21:09 +0530

    feat: Added firebase for image upload, added create new Product functionality.

--- src/components/ProductForm/ProductForm.jsx
+++ src/components/ProductForm/ProductForm.jsx
@@ -1,41 +1,109 @@
 import './ProductForm.css'
+import { useState } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { addProduct } from '../../service/productsData';
+import app from '../../firebaseConfig';
 
 const ProductForm = () => {
 
+    const [ formInputs, setFormInputs ] = useState({
+        title: "",
+        desc: "",
+        price: 1000,
+        inStock: "true"
+    });
+    const [file, setFile] = useState("");
 
     const handleChange = () => {}
+
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const fileName = new Date().getTime() + file.name;
+        const storage = getStorage(app);
+        const storageRef = ref(storage, fileName);
+    
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed', 
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            }
+          }, 
+          (error) => {
+            // Handle unsuccessful uploads
+            console.log(error);
+          }, 
+          () => {
+            // Handle successful uploads on complete
+            
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log('File available at', downloadURL);
+            //   console.log({...formInputs, categories: cat, img: downloadURL});
+            //   const product = {...formInputs, categories: cat, img: downloadURL};
+              const product = {...formInputs, img: downloadURL};
+              addProduct(product);
+          });
+        }
+        );
+      }
+
 
     return (
         <div className="productForm">
             <form>
                 <div className="productForm_item">
                     <label>Image</label>
-                    <input type="file" id="file" />
+                    <input type="file" id="file" onChange={(e) => {
+                        setFile(e.target.files[0])
+                    }} />
                 </div>
+                
                 <div className="productForm_item">
                     <label>Title</label>
-                    <input name="title" type="text" placeholder="Optimum Nutrition Whey Protein" />
+                    <input name="title" type="text" placeholder="Optimum Nutrition Whey Protein" 
+                    value={formInputs.title}
+                    onChange={(e) => setFormInputs(prev => ({...prev, title: e.target.value}))}/>
                 </div>
+                
                 <div className="productForm_item">
                     <label>Price</label>
-                    <input name="price" type="number" placeholder="1000" />
+                    <input name="price" type="number" placeholder="1000" 
+                    value={formInputs.price}
+                    onChange={(e) => setFormInputs(prev => ({...prev, price: e.target.value}))}/>
                 </div>
+
                 <div className="productForm_item">
                     <label>Description</label>
-                    <input name="desc" type="text" placeholder="Product description" />
+                    <input name="desc" type="text" placeholder="Product description" 
+                    value={formInputs.desc}
+                    onChange={(e) => setFormInputs(prev => ({...prev, desc: e.target.value}))}/>
                 </div>
-                <div className="productForm_item">
+                
+                {/* <div className="productForm_item">
                     <label>Categories</label>
                     <input type="text" placeholder="whey, pre-workout, creatine, chocolate" />
-                </div>
+                </div> */}
+
                 <div className="productForm_item">
                     <label>Stock</label>
-                    <select name="inStock" onChange={handleChange}>
+                    <select name="inStock" value={formInputs.inStock}
+                    onChange={(e) => setFormInputs(prev => ({...prev, inStock: e.target.value}))} >
                         <option value="true">Yes</option>
                         <option value="false">No</option>
                     </select>
                 </div>
-        
+
+                <button type="button" className="productForm_addProductButton" onClick={handleClick}>
+                    Add Product
+                </button>
             </form>
         </div>
     )
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-2tok1vJn3gn8dS_bYUwVwt07Cc5WXno",
+  authDomain: "shop-aa8dd.firebaseapp.com",
+  projectId: "shop-aa8dd",
+  storageBucket: "shop-aa8dd.appspot.com",
+  messagingSenderId: "201014988618",
+  appId: "1:201014988618:web:6e6d5f9f70f7e9f37bfbb3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
